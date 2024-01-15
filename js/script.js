cd93c6bcec02a4bf69909b079b6712beac04db72
Polvere243 <danielercoli@gmail.com>
2024-01-15 15:55:12 +0100

    completed first two milestones

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log("JS OK");
+// creo un array di oggetti
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+for (let i = 0; i < teamMembers.length; i++) {
+    console.log(teamMembers[i]);
+}
