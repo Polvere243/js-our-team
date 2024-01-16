f902e52c50c0f96af7bc4e4b29e28accd970b2e8
Polvere243 <danielercoli@gmail.com>
2024-01-16 01:31:20 +0100

    once again failed

--- js/script.js
+++ js/script.js
@@ -1,37 +1,45 @@
 // creo un array di oggetti
 const teamMembers = [
     {
-        name: "Wayne Barnett",
+        fullname: "Wayne Barnett",
         role: "Founder & CEO",
         image: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        name: "Angela Caroll",
+        fullname: "Angela Caroll",
         role: "Chief Editor",
         image: "angela-caroll-chief-editor.jpg"
     },
     {
-        name: "Walter Gordon",
+        fullname: "Walter Gordon",
         role: "Office Manager",
         image: "walter-gordon-office-manager.jpg"
     },
     {
-        name: "Angela Lopez",
+        fullname: "Angela Lopez",
         role: "Social Media Manager",
         image: "angela-lopez-social-media-manager.jpg"
     },
     {
-        name: "Scott Estrada",
+        fullname: "Scott Estrada",
         role: "Developer",
         image: "scott-estrada-developer.jpg"
     },
     {
-        name: "Barbara Ramos",
+        fullname: "Barbara Ramos",
         role: "Graphic Designer",
         image: "barbara-ramos-graphic-designer.jpg"
     }
 ];
 
+
+/* 
 for (let i = 0; i < teamMembers.length; i++) {
     console.log(teamMembers[i]);
+} */
+
+for (let i = 0; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+    console.log(member);
+    for (let member in teamMembers)
 }
