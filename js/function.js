5c5bdeb37725db9619159245f7402b18c1f56e0f
Polvere243 <danielercoli@gmail.com>
2024-01-20 23:10:04 +0100

    created a function to print in page the infos and images

--- js/function.js
+++ js/function.js
@@ -1,3 +1,13 @@
-const createList = () => {
-
+const createList = (member) => {
+    
+   let list = `<ul class="list">`;
+   teamMembers.forEach (member => {
+    list += `<li>${member.fullname}</li>
+    <li>${member.role}</li>
+    <li><img src="img/${member.image}" alt="member.fullname"></li>`;
+   })
+   
+   
+   list += `</ul>`;
+   sectionElement.innerHTML = list;
 }
